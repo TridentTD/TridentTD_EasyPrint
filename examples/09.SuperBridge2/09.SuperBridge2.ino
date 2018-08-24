2d09035268beed9748e22e93297e0b4f6ada2811
TridentTD <prote2000@gmail.com>
2018-08-24 15:04:36 +0700

    update example 9 with comment

--- examples/09.SuperBridge2/09.SuperBridge2.ino
+++ examples/09.SuperBridge2/09.SuperBridge2.ino
@@ -1,64 +1,35 @@
 #include <TridentTD_EasyPrint.h>
 #include <SoftwareSerial.h>
-#include <DHT.h>
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 
-// DHT Sensor config
-#define DHT_PIN     4
-#define DHT_TYPE    DHT11
-DHT  dht(DHT_PIN, DHT_TYPE);
-
-// อุปกรณ์รอบข้างต่างๆ ต่อแบบ SoftwareSerial เช่น Bluetooth เป็นต้น
-SoftwareSerial myDevice1(4,5);
-SoftwareSerial myDevice2(10,11);
+SoftwareSerial R232Serial(4,5);  //กำหนดขา rx,tx
 
 // Ethernet config
-IPAddress ether_ip      = IPAddress(192, 168, 43, 177);
-uint16_t  ether_port    = 8888;
-byte      ether_mac[]   = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ether_ip      = IPAddress(192, 168, 43, 177); // กำหนด IP ของ Ethernet
+uint16_t  ether_port    = 8888;                         // กำหนด Port ของ Ethernet
+byte      ether_mac[]   = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //กำหนด mac address
 
 // UDP Server config
-IPAddress udpserver_ip  = IPAddress(192, 168, 43, 1);
-uint16_t  udpserver_port= 12345;
+IPAddress udpserver_ip  = IPAddress(192, 168, 43, 1); // กำหนด IP ขอว UDP Server ที่ต้องการ
+uint16_t  udpserver_port= 12345;                      // กำหนด Port ของ UDP Server
 
 EthernetUDP Udp;
 
 void setup(){
-  Serial.begin(115200);
-  myDevice1.begin(9600);
-  myDevice2.begin(9600);
-
-  dht.begin();
-  Ethernet.begin(ether_mac, ether_ip);
-  Udp.begin(ether_port);
+  Serial.begin(115200);                 // เริ่มต้นใช้งาน Serial Monitor
+  R232Serial.begin(9600);               // เริ่มต้นใช้งาน R232Serial
+  Ethernet.begin(ether_mac, ether_ip);  // เริ่มต้นเชื่อมต่อ Ethernet
+  Udp.begin(ether_port);                // เริ่มต้นใช้งาน UDP Client ของ Ethernet
 }
 
-uint32_t timer;
 
 void loop(){
-  if( millis() -timer >= 2000) { // อ่านค่า sensor และส่ง ในทุก 2 วินาที
-    timer = millis();
-
-    // อ่านค่า DHT Sensor
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
-    while ( isnan(h) || isnan(t) ) {
-      h = dht.readHumidity();
-      t = dht.readTemperature();
-    }
-
-    // เตรียมข้อมูล จาก sensor จัดแบบ String กำหนด format แบบ json
-    char buf[50];
-    sprintf(buf, "{\"humid\":%.2f,\"temperature\":%.2f}", h, t);
-    String data_json = String(buf) + "\n";
-
-    // ทำการส่ง String แสดงผลบน Serial Monitor 
-    // พร้อมทั้งส่งไปตาม myDevice ต่างๆ จนครบ
-    // และส่งไปยัง Udp Server ที่ทำ IoT ให้ด้วย
-    Udp.beginPacket(udpserver_ip, udpserver_port);
-    data_json >> Serial >> myDevice1 >> myDevice2 >> Udp;
+  if( R232Serial.available()) {
+    Udp.beginPacket(udpserver_ip, udpserver_port);  // เชื่อมต่อไปยัง UDP Server ที่ต้องการ
+    // หากมีข้อมูลมาจาก R232Serial จะทำการส่งต่อไปแสดงที่ Serial Monitor พร้อมทั้งส่งขึ้นไปยัง Udp Server ให้ด้วย
+    R232Serial >> Serial >> Udp;
     Udp.endPacket();
   }
 }
