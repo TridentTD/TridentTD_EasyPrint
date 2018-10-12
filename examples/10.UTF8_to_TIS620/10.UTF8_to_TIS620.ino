c75ae650dab8e5872ca7dc026373a61e730b1024
TridentTD <prote2000@gmail.com>
2018-10-12 15:30:00 +0700

    Update examples/10.UTF8_to_TIS620/10.UTF8_to_TIS620.ino

--- examples/10.UTF8_to_TIS620/10.UTF8_to_TIS620.ino
+++ examples/10.UTF8_to_TIS620/10.UTF8_to_TIS620.ino
@@ -10,6 +10,9 @@ void setup() {
   
   Serial.println( thai_utf8_str );
   Serial.println( thai_tis620_str);
+
+  for(int i =0; i < thai_tis620_str.length(); ++i)
+    Serial << "[charAt " << i << "] code = " << (int)thai_tis620_str[i] << endl;
 }
 
 void loop() {
