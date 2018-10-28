5dc1c2da50569031cdb3a3dd3e754882ccbcfb9a
TridentTD <prote2000@gmail.com>
2018-10-28 12:20:39 +0700

    add UTF8_code and TIS620_code

--- src/TridentTD_EasyPrint.cpp
+++ src/TridentTD_EasyPrint.cpp
@@ -79,5 +79,14 @@ String TridentTD::TIS620_to_UTF8(String tis620_str, bool debug){
   return utf8_str;
 }
 
+uint32_t TridentTD::UTF8_code(char c) {
+  String u =  TridentTD::TIS620_to_UTF8(TridentTD::UTF8_to_TIS620(String(c))); 
+  uint32_t code = *(uint32_t*) u.c_str(); 
+  return ((code & 0xFF) << 16)  | (code & 0xFF00) | ((code >> 16 ) & 0xFF);
+}
+
+uint8_t TridentTD::TIS620_code(char c){ 
+  return *(uint8_t*) TridentTD::UTF8_to_TIS620(String(c)).c_str();
+}
 
 #endif //__TRIDENTTD_SPACE___
--- src/TridentTD_EasyPrint.h
+++ src/TridentTD_EasyPrint.h
@@ -186,6 +186,8 @@ inline String operator >> (Stream &obj1, Stream &obj2)
 namespace TridentTD {
   String UTF8_to_TIS620(String utf8_str, bool debug = false);
   String TIS620_to_UTF8(String tis620_str, bool debug = false);
+  uint32_t  UTF8_code(char c);
+  uint8_t   TIS620_code(char c);
 }
 
 
